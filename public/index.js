388cd1d22969c1f3efe69ddfbb85646b509fdafb
vladyslavnUA <vladyslavn@yahoo.com>
2020-10-21 01:11:32 -0400

    users can log in

--- public/index.js
+++ public/index.js
@@ -1,4 +1,20 @@
-$(document).ready( () => {
-    // connect to the socket.io server
+$(document).ready(() => {
+
     const socket = io.connect();
-  })
+  
+    $('#create-user-btn').click((e) => {
+      e.preventDefault();
+      let username = $('#username-input').val();
+      if(username.length > 0){
+        //Emit to the server the new user
+        socket.emit('new user', username);
+        $('.username-form').remove();
+      }
+    });
+  
+    //socket listeners
+    socket.on('new user', (username) => {
+      console.log(`✋ ${username} has joined the chat! ✋`);
+    })
+  
+})
