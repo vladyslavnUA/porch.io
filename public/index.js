737b978d524f209fe53d1c6e895d8afaa760052c
vladyslavnUA <vladyslavn@yahoo.com>
2020-10-21 01:25:31 -0400

    users can join and leave chats

--- public/index.js
+++ public/index.js
@@ -1,8 +1,7 @@
 $(document).ready(()=>{
     const socket = io.connect();
-  
-    //Keep track of the current user
     let currentUser;
+    socket.emit('get online users');
   
     $('#create-user-btn').click((e)=>{
       e.preventDefault();
@@ -44,5 +43,19 @@ $(document).ready(()=>{
           </div>
         `);
       })
-  
+
+    socket.on('get online users', (onlineUsers) => {
+        //You may have not have seen this for loop before. It's syntax is for(key in obj)
+        //Our usernames are keys in the object of onlineUsers.
+        for(username in onlineUsers){
+          $('.users-online').append(`<div class="user-online">${username}</div>`);
+        }
+    })
+
+    socket.on('user has left', (onlineUsers) => {
+        $('.users-online').empty();
+        for(username in onlineUsers){
+          $('.users-online').append(`<p>${username}</p>`);
+        }
+    });
 })
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,15 +1,28 @@
-module.exports = (io, socket) => {
+module.exports = (io, socket, onlineUsers) => {
 
     socket.on('new user', (username) => {
-      console.log(`✋ ${username} has joined the chat! ✋`);
-      io.emit("new user", username);
+        // save the username as key to access the user's socket id
+        onlineUsers[username] = socket.id;
+        // save the username to socket as well. This is important for later.
+        socket["username"] = username;
+        console.log(`✋ ${username} has joined the chat! ✋`);
+        io.emit("new user", username);
     })
   
-    //Listen for new messages
+    // listen for new messages
     socket.on('new message', (data) => {
-      // Send that data back to ALL clients
-      console.log(`🎤 ${data.sender}: ${data.message} 🎤`)
-      io.emit('new message', data);
+        // send that data back to ALL clients
+        console.log(`🎤 ${data.sender}: ${data.message} 🎤`)
+        io.emit('new message', data);
+    })
+
+    socket.on('get online users', () => {
+        socket.emit('get online users', onlineUsers);
     })
   
+    socket.on('disconnect', () => {
+        //This deletes the user by using the username we saved to the socket
+        delete onlineUsers[socket.username]
+        io.emit('user has left', onlineUsers);
+    });
 }
